5f04adb71bbca55b63342b9dccdf4ca3c22badc7
BodomMoon <BodomMoon@gmail.com>
2018-03-09 04:50:38 +0800

    Update with HashTable and optimize code
         Eliminate unnecessary loop and value
         Update check performence more accurate and add hash table
         Update table size to 4096 and combine cPtr++ to previous commond
         Separated the block that free opt and orig

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 #include <assert.h>
 
 #include IMPL
+#define MAX_TABLE_SIZE 4096
 
-#ifdef OPT
+#if OPT == 1
 #define OUT_FILE "opt.txt"
+
+entry *hashTable[MAX_TABLE_SIZE] = {NULL};
 #else
 #define OUT_FILE "orig.txt"
 #endif
@@ -27,10 +30,21 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
+unsigned int BKDRhash(char *cPtr)
+{
+    int hash = 0;
+    int seed = 31; 
+    do
+        hash = hash * seed + (*cPtr++);
+    while(*cPtr != '\0');
+    return (hash & (MAX_TABLE_SIZE -1)); //we learn from class
+}
+
 int main(int argc, char *argv[])
 {
+
     FILE *fp;
-    int i = 0;
+    //int i = 0;
     char line[MAX_LAST_NAME_SIZE];
     struct timespec start, end;
     double cpu_time1, cpu_time2;
@@ -54,33 +68,43 @@ int main(int argc, char *argv[])
 #endif
     clock_gettime(CLOCK_REALTIME, &start);
     while (fgets(line, sizeof(line), fp)) {
-        while (line[i] != '\0')
-            i++;
-        line[i - 1] = '\0';
-        i = 0;
+        line[strlen(line) -1] = '\0'; //change while loop to strlen
+#if OPT == 1 //compilering OPT version
+        unsigned int hashValue = BKDRhash(line);
+        if(hashTable[hashValue] == NULL){
+            hashTable[hashValue] =(entry*) malloc(sizeof(entry)); 
+        }
+        append(line , hashTable[hashValue]);  
+#else       //compilering orgin version  
         e = append(line, e);
+#endif
     }
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time1 = diff_in_second(start, end);
 
     /* close file as soon as possible */
     fclose(fp);
-
-    e = pHead;
-
     /* the givn last name to find */
     char input[MAX_LAST_NAME_SIZE] = "zyxel";
+
+#if OPT == 1 //compilering OPT version
+    e = hashTable[BKDRhash(input)];
+#else
     e = pHead;
+#endif
 
     assert(findName(input, e) &&
            "Did you implement findName() in " IMPL "?");
     assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
+
+
 
 #if defined(__GNUC__)
     __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
     /* compute the execution time */
     clock_gettime(CLOCK_REALTIME, &start);
+    //printf("%s",findName(input, e)->lastName );
     findName(input, e);
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time2 = diff_in_second(start, end);
@@ -92,8 +116,22 @@ int main(int argc, char *argv[])
     printf("execution time of append() : %lf sec\n", cpu_time1);
     printf("execution time of findName() : %lf sec\n", cpu_time2);
 
-    if (pHead->pNext) free(pHead->pNext);
-    free(pHead);
+#if OPT == 1 //compilering OPT version
+    int counter = 0;
+    do
+    {
+        free(hashTable[counter] -> pNext);
+        free(hashTable[(counter++)] );
+
+    }while(counter < MAX_TABLE_SIZE);
+#else
+    if (pHead != NULL) {
+        free(pHead->pNext);
+        free(pHead);
+    }
+#endif
 
     return 0;
 }
+
+
